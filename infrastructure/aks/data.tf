0e2601c73300513c35949c62c7ca68dc85167b83
Leticia Valladares <leticia.valladares@cloudreach.com>
2024-04-04 15:42:50 +0200

    Correct vnet and rg name in data source

--- infrastructure/aks/data.tf
+++ infrastructure/aks/data.tf
@@ -1,5 +1,5 @@
 data "azurerm_subnet" "node_snet" {
   name                 = "${local.trainee_name_validated}-${local.name_suffix}-2-snet"
-  resource_group_name  = "${local.trainee_name_validated}-${local.name_suffix}-rg"
+  resource_group_name  = "${local.name_suffix}-vnet-rg"
   virtual_network_name = "${local.name_suffix}-vnet"
 }
