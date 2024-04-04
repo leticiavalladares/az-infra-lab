3ca6bda5f4476ffd396bdc91b565dd237124ee5b
Leticia Valladares <leticia.valladares@cloudreach.com>
2024-04-04 15:26:36 +0200

    Delete unnecesary script

--- scripts/destroy-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/az-infra-lab/infrastructure/aks
-terraform destroy -auto-approve
-echo "Terraform destroy for AKS completed"
-cd ~/az-infra-lab/infrastructure
-terraform destroy -auto-approve
-echo "Terraform destroy for the rest of resources completed"
