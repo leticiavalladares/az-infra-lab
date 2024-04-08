f4ddf22f2e5a10ca7152ccd7e59109cc0edd2937
Leticia Valladares <leticia.valladares@cloudreach.com>
2024-04-08 18:20:46 +0200

    Add cluster conversion with az cli

--- infrastructure/aks/priv-cluster-setup/udr-cluster.tf
+++ infrastructure/aks/priv-cluster-setup/udr-cluster.tf
@@ -1,29 +1,3 @@
-data "azurerm_public_ip" "bastion_pip" {
-  name                = "${local.name_suffix}-pip"
-  resource_group_name = data.azurerm_resource_group.vnet_rg.name
-}
-
-resource "azurerm_route_table" "rt" {
-  name                          = "${local.name_suffix}-rt"
-  location                      = data.azurerm_resource_group.vnet_rg.location
-  resource_group_name           = data.azurerm_resource_group.vnet_rg.name
-  disable_bgp_route_propagation = false
-
-  route {
-    name                   = "${local.name_suffix}-aks-route-1"
-    address_prefix         = "0.0.0.0/0"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = data.azurerm_public_ip.bastion_pip.ip_address
-  }
-
-  tags = local.default_tags
-}
-
-resource "azurerm_subnet_route_table_association" "snet_rt_assoc" {
-  subnet_id      = data.azurerm_subnet.node_snet.id
-  route_table_id = azurerm_route_table.rt.id
-}
-
 resource "azurerm_kubernetes_cluster" "cluster" {
   for_each = local.clusters
 
@@ -58,7 +32,6 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
     load_balancer_profile {
       idle_timeout_in_minutes = 0
-      outbound_ip_address_ids = null
     }
   }
 
--- scripts/deploy-aks.sh
+++ scripts/deploy-aks.sh
@@ -5,6 +5,9 @@ tf_init=$(terraform init)
 echo $tf_init
 echo "Terraform init started"
 terraform apply -auto-approve
+mv ./priv-cluster-setup/route-table.tf .
+terraform apply -auto-approve
+az aks update -g $TF_VAR_trainee_name-eon-lab-gwc-aks-rg -n $TF_VAR_trainee_name-eon-lab-gwc-1-aks --outbound-type userDefinedRouting
 mv ./priv-cluster-setup/udr-cluster.tf .
 mv ./lb-cluster.tf ./priv-cluster-setup/
 terraform apply -auto-approve
