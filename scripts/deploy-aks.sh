d28215a754e639a9e7c8579d5cf2302eb248f935
Leticia Valladares <leticia.valladares@cloudreach.com>
2024-04-12 18:38:04 +0200

    Correct script to create win repo on ACR

--- scripts/deploy-aks.sh
+++ scripts/deploy-aks.sh
@@ -9,7 +9,9 @@ cd ./acr-images/linux/
 ACR_RGNAME=$TF_VAR_trainee_name-eon-lab-gwc-aks-rg
 ACR_NAME=$(az acr list --resource-group $ACR_RGNAME --query "[].name" --output tsv)
 az acr build --registry $ACR_NAME --image hellofromnode:v1.0 .
-cd ../windows/
+cd ..
+mkdir windows
+cd ./windows/
 git clone https://github.com/Azure-Samples/dotnetcore-docs-hello-world.git
 cd ./dotnetcore-docs-hello-world/
 az acr build --registry $ACR_NAME --image hellofromdotnet:v1.0 --platform windows --file Dockerfile.windows .
